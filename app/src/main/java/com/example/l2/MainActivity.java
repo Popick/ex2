660acef82332d33de11b20573d7e44bdefd5ecbe
BuildTools <unconfigured@null.spigotmc.org>
2021-01-16 22:37:07 +0200

    code

---
+++ app/src/main/java/com/example/l2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.l2;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImg(View view) {
+
+        Button btn1 = (Button) findViewById(R.id.btn);
+        ImageView iv = (ImageView) findViewById(R.id.img);
+
+        //אלברט אם אני כותב את ה2 שורות מעל מחוץ לפונקציה האפליקציה לא נפתחת, אפשר לדעת למה?
+
+        int rand = (int)(Math.random() * 3) + 1;
+        System.out.print(rand);
+        if (rand==1) {
+            iv.setImageResource(R.drawable.fbam);
+        }else if(rand==2){
+            iv.setImageResource(R.drawable.rbam);
+        }else if(rand==3){
+            iv.setImageResource(R.drawable.nbam);
+        }
+        btn1.setText("Y̷̰̏Ȍ̷͎U̶̲̿R̴̜͐ ̷̫͠N̴̺̅Ů̸̻M̶̪̈́B̴̮͛Ę̵̆Ŗ̸͆ ̶̹͘I̶̫͝S̶͓̿:̶̣̫̜̤̭̖̙͑̾͌̌̀̓̎̌̕ "+ rand);
+    }
+
+}
